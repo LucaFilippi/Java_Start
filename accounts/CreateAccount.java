0e2d6ac99577b40f4f7d2816d6b8aca6cc965a7a
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-26 12:55:42 -0300

    OOP studying

--- accounts/CreateAccount.java
+++ accounts/CreateAccount.java
@@ -4,6 +4,21 @@ public class CreateAccount {
     public static void main(String[] args) {
         Account firstAccount = new Account();
         firstAccount.balance = 200;
-        System.out.println(firstAccount.balance);
+        System.out.println(" the first account balance is " + firstAccount.balance + " dollars");
+
+        firstAccount.balance += 100;
+        System.out.println(" the first account balance is " + firstAccount.balance + " dollars");
+
+        Account secondAccount = new Account();
+        secondAccount.balance = 50;
+
+
+        System.out.println(" the first account balance is " + firstAccount.balance + " dollars");
+        System.out.println(" the first account balance is " + secondAccount.balance + " dollars");
+
+
+
+
+
     }
 }
