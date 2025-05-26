b512541ffe8a78fdf438bbc92e0f0cc67b33caab
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-26 10:36:32 -0300

    testing loops

---
+++ WhileTest.java
@@ -0,0 +1,21 @@
+public class WhileTest{
+    public static void main(String[] args){
+        
+        int counter;
+        counter = 0;
+
+        while(counter < 10 ){
+            System.out.println(counter);
+            //counter = counter + 1;
+            //counter += 1; 
+            counter ++;
+
+        }
+        System.out.println(counter);
+
+
+
+    }
+
+
+}
