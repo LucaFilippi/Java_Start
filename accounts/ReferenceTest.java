fab0f887a37f7c307ed3b69af12eea28e15fdf3d
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-26 13:22:09 -0300

    reference and variables

---
+++ accounts/ReferenceTest.java
@@ -0,0 +1,34 @@
+package accounts;
+
+public class ReferenceTest {
+
+    public static void main(String[] args) {
+        Account firstAccount = new Account();
+        firstAccount.balance = 300;
+
+        System.out.println("the first account balace is " + firstAccount.balance);
+
+        Account secondAccount = firstAccount;
+
+        System.out.println("the second account balace is " + secondAccount.balance);
+
+        secondAccount.balance += 100;
+
+        System.out.println("the second account balace is " + secondAccount.balance);
+        System.out.println("the first account balace is " + firstAccount.balance);
+
+        if (firstAccount == secondAccount) {
+            System.out.println("They are the same account"); // just to show that, it can have the same name, but its only one object
+
+        } else {
+            System.out.println("are not the same account");
+
+        }
+
+        System.out.println(firstAccount);
+        System.out.println(secondAccount); //shows the same path for both variables
+
+    }
+
+}
+// reference != variable 
