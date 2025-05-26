782a78dc8800f4cd3fda59f17e9c7ec7bc9bd5fd
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-25 21:28:46 -0300

    integers in Java

---
+++ variables.java
@@ -0,0 +1,20 @@
+public class integer{
+    public static void main(String[] args){
+        int age;
+        age = 13;
+        // int age = 13;
+        System.out.println(age);
+
+        age = 13 - 11;
+        System.out.println(age);
+
+        age = 13 + 11;
+        System.out.println(age);
+
+        age = 13 / 1;
+        System.out.println(age);
+
+        age = 13 * 11;
+        System.out.println(age);
+    }
+}
