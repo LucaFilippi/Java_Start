f4f25a3969b1091fc8398ab87cfeb568132c35f2
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-26 10:42:14 -0300

    loops tests and correction

--- WhileTest.java
+++ WhileTest.java
@@ -1,21 +1,20 @@
-public class WhileTest{
-    public static void main(String[] args){
-        
+
+public class WhileTest {
+
+    public static void main(String[] args) {
+
         int counter;
         counter = 0;
 
-        while(counter < 10 ){
+        while (counter < 10) {
             System.out.println(counter);
             //counter = counter + 1;
             //counter += 1; 
-            counter ++;
+            counter++;
 
         }
         System.out.println(counter);
 
-
-
     }
-
 
 }
