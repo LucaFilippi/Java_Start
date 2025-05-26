407afc83a7b906a14241972d8da3368c817be558
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-25 21:35:44 -0300

    sum and concatenation

---
+++ ages_sum.java
@@ -0,0 +1,11 @@
+public class sum{
+    public static void main(String[] args){
+        int age1, age2, sum;
+        age1 = 13;
+        age2 = 11;
+
+        sum = age1 + age2;
+
+        System.out.println("the sum of the ages is " + sum);
+    }
+}
