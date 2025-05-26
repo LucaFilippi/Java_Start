58b7edc35eb8d5e669df51348091b289fe523114
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-25 22:39:31 -0300

    variables test

---
+++ VariableTest.java
@@ -0,0 +1,15 @@
+public class VariableTest{
+    public static void main(String[] args){
+        int first, second;
+        first = 5;
+        second = 7;
+
+        second = first;
+        first = 10; //this line doesn't change the value of 'second' since it executes after the initial assignment 
+        
+        System.out.println(second);
+
+
+    }
+
+}
