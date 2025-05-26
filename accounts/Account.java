63b32bfee6abe59f8b4059c380ede7e1fcfe9413
Luca Francesco Filippi <luca.ffilippi@gmail.com>
2025-05-26 13:44:53 -0300

    metods testing

--- accounts/Account.java
+++ accounts/Account.java
@@ -5,4 +5,10 @@ public class Account {
     double balance;
     int branch, number;
     String holder;
+
+    public void deposit(double value){
+        balance = balance + value; 
+        //this.balance = this.balance + value 
+        //"this" serves to show that the variable is not local for the function, but the account.
+    }
 }
---
+++ accounts/MetodTest.java
@@ -0,0 +1,13 @@
+package accounts;
+
+public class MetodTest{
+
+    public static void main(String[] args){
+        Account lucasaccount = new Account();
+        lucasaccount.balance = 100;
+        lucasaccount.deposit(50);
+
+System.out.println(lucasaccount.balance);
+
+    }
+}
